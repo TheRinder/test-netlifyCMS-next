3f02ceebcf41a0c48a036d0831b3847e52bc76bf
The Rinder <rinder500@gmail.com>
2022-12-21 15:44:21 +0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: {
+    locales: ['ru', 'en', 'de', 'it'],
+    defaultLocale: 'en'
+  },
   reactStrictMode: true,
+
+  webpack: (cfg) => {
+    cfg.module.rules.push(
+      {
+        test: /\.md$/,
+        loader: 'frontmatter-markdown-loader',
+        options: { mode: ['react-component'] }
+      }
+    )
+    return cfg;
+  }
 }
 
 module.exports = nextConfig
