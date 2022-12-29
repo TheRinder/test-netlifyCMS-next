02f3a9523df88197fad767315f58c6bc80a7eab4
The Rinder <rinder500@gmail.com>
2022-12-29 20:07:15 +0300

    post style - done

--- src/components/Areas/Post.tsx
+++ src/components/Areas/Post.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import style from './areas.module.scss'
-// import { attributes, react as PostContent } from '../../../content/blog/business-planning.md'
-
 
 export const PostContainer = ({ children }: { children?: React.ReactNode }) => {
  return (
   <div className={style.PostContainer}>
-   <div className={style.PostContainerBg}>
+   <div className={style.PostContent}>
     {children}
    </div>
   </div>
