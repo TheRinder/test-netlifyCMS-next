902a495dee921198396f2f8f68908d453d9986c4
The Rinder <rinder500@gmail.com>
2023-01-11 19:38:02 +0300

    add new partner, add anim packege card, news render

--- src/components/Home/PartnersSection.tsx
+++ src/components/Home/PartnersSection.tsx
@@ -4,44 +4,48 @@ import logoPlaceholder from '../../icon/LOGO.png'
 import clsx from 'clsx'
 
 interface Partner {
- name: string,
- logo: string,
+  name: string,
+  logo: string,
 }
 const PARTNETS: Partner[] = [
- {
-  name: 'Placeholder',
-  logo: './image/kandkLogo.png'
- },
- {
-  name: 'Placeholder',
-  logo: './image/logo.png'
- }
+  {
+    name: 'ten',
+    logo: './image/logo.png'
+  },
+  {
+    name: 'kandk',
+    logo: './image/kandkLogo.png'
+  },
+  {
+    name: "lhv",
+    logo: './image/lhv-logo.png'
+  }
 ]
 
 const PartnerCard = (props: Partner) => {
- const { name, logo } = props
- const imgUrl = logo.length === 0 ? logoPlaceholder.src : logo
- return (
-  <div className={style.partnerItem}>
-   <img src={imgUrl} alt={name} />
-  </div>
- )
+  const { name, logo } = props
+  const imgUrl = logo.length === 0 ? logoPlaceholder.src : logo
+  return (
+    <div className={style.partnerItem}>
+      <img src={imgUrl} alt={name} />
+    </div>
+  )
 }
 
 export const PartnersSection = () => {
   const classes = clsx('sectionTitle', style.partnerTitle)
- return (
-  <section className={style.partnersBox}>
-   <div className="container">
-    <h2 className={classes}>
-     Our Partners
-    </h2>
-    <div className={style.partnersGrid}>
-     {
-      PARTNETS.map((item, i) => <PartnerCard key={i} {...item} />)
-     }
-    </div>
-   </div>
-  </section>
- )
+  return (
+    <section className={style.partnersBox}>
+      <div className="container">
+        <h2 className={classes}>
+          Our Partners
+        </h2>
+        <div className={style.partnersGrid}>
+          {
+            PARTNETS.map((item, i) => <PartnerCard key={i} {...item} />)
+          }
+        </div>
+      </div>
+    </section>
+  )
 }
