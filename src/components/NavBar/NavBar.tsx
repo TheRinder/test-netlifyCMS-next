f0a95208522ece55197a60e2846d910f78687396
The Rinder <rinder500@gmail.com>
2023-01-17 18:18:29 +0300

    update favicon

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -8,13 +8,55 @@ const VisitorAPI = require("visitorapi");
 import { LocationIcon } from '../../icon/headerIcon'
 import { useRouter } from 'next/router'
 
+type NavLink = {
+  subMenu?: ({ subMenu?: NavItem[] } & NavItem)[]
+} & NavItem
 
-interface NavLink {
+interface NavItem {
   id: string,
   title: string,
   href: string,
   isActive?: boolean,
 }
+
+const NAV_LINKS: NavLink[] = [
+  {
+    id: 'services',
+    title: 'Services',
+    href: '/services/business/business-planning',
+    subMenu: [
+      {
+        id: '',
+        title: '',
+        href: '',
+        subMenu: [
+          {
+            id: '',
+            title: '',
+            href: '',
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: 'about',
+    title: 'About Us',
+    href: '/about'
+  },
+  {
+    id: 'news',
+    title: 'News',
+    href: '/news'
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+    href: '/#contact'
+  },
+]
+
+
 
 const NavLink = (props: NavLink) => {
   const { title, href } = props
@@ -38,41 +80,30 @@ const IconAndButton = ({ text, Icon }: { text: string, Icon: React.FunctionCompo
   )
 }
 
-const NavMenuMobile = () => {
+const NavMenuMobile = ({ open, pathname, setOpen }: { setOpen: (boolean) => void, open: boolean, pathname: string }) => {
   return (
-    <div>
-
+    <div className={clsx(scss.navMobile, open && scss.openNavMobile)}>
+      <nav className={scss.navMobileBox}>
+        <ul>
+          {
+            NAV_LINKS.map((link, i) =>
+              <NavLink
+                {...link}
+                key={link.id}
+                isActive={pathname.split('/')[1] === link.id}
+              />
+            )
+          }
+        </ul>
+      </nav>
+      <div className={scss.navMobileBtnClose}>
+        <button onClick={() => { setOpen(false) }}>
+          Cancel
+        </button>
+      </div>
     </div>
   )
 }
-
-const NAV_LINKS: NavLink[] = [
-  {
-    id: 'services',
-    title: 'Services',
-    href: '/services/business/business-planning'
-  },
-  // {
-  //   id: 'areas',
-  //   title: "Areas",
-  //   href: "/areas/"
-  // },
-  {
-    id: 'about',
-    title: 'About Us',
-    href: '/about'
-  },
-  {
-    id: 'news',
-    title: 'News',
-    href: '/news'
-  },
-  {
-    id: 'contact',
-    title: 'Contact',
-    href: '/#contact'
-  },
-]
 
 const MenuIcon = () => {
   return (
@@ -89,8 +120,6 @@ export const NavBar = () => {
   const [openMenu, setOpenMenu] = React.useState<boolean>(false)
 
   const menuOnChange = () => {
-    console.log(openMenu);
-
     setOpenMenu(!openMenu)
   }
 
@@ -137,22 +166,7 @@ export const NavBar = () => {
           {/* <IconAndButton text={locale} Icon={LangIcon} /> */}
         </div>
       </div>
-      <div className={clsx(scss.navMobile, openMenu && scss.openNavMobile)}>
-        <nav className={scss.navMobileBox}>
-          <ul>
-            {
-              NAV_LINKS.map((link, i) => <NavLink key={link.id} {...link} isActive={pathname.split('/')[1] === link.id} />)
-            }
-            {/* <li className={scss.navMobileBtnClose}>
-              <button onClick={() => { setOpenMenu(false) }}>
-                <svg style={{ transform: 'rotateY(180deg)' }} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path fillRule="evenodd" clipRule="evenodd" d="M3.70716 11.1771C3.3078 11.2321 3 11.5795 3 11.9999C3 12.4585 3.36631 12.8302 3.81818 12.8302H18.1999L13.0047 18.0813L12.9253 18.1743C12.687 18.4989 12.7124 18.9602 13.0023 19.2556C13.3212 19.5805 13.8392 19.5816 14.1594 19.258L20.7477 12.5996C20.787 12.5614 20.8224 12.5194 20.8536 12.474C21.0766 12.1498 21.0452 11.6999 20.7593 11.4111L14.1593 4.74193L14.0674 4.66174C13.7466 4.42125 13.2921 4.44905 13.0023 4.74446C12.6834 5.06942 12.6845 5.59515 13.0047 5.91871L18.2012 11.1696L3.81818 11.1696L3.70716 11.1771Z" fill="#7854F7" />
-                </svg>
-              </button>
-            </li> */}
-          </ul>
-        </nav>
-      </div>
+      <NavMenuMobile open={openMenu} pathname={pathname} setOpen={setOpenMenu} />
     </header>
   )
 }
